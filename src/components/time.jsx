80990beb4ba62c70cd20e17729df264e7e5c30f0
Omar Salam <omar-ma-salam@outlook.com>
2018-12-01 15:29:37 +0400

    Promote to class

--- src/components/time.jsx
+++ src/components/time.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function getTimeString({ min, sec }) {
-  const m = min < 10 ? `0${min}` : min;
-  const s = sec < 10 ? `0${sec}` : sec;
-  return `${m} : ${s}`;
+class Time extends Component {
+  getTimeString({ min, sec }) {
+    const m = min < 10 ? `0${min}` : min;
+    const s = sec < 10 ? `0${sec}` : sec;
+    return `${m} : ${s}`;
+  }
+
+  render() {
+    const { time, paused, onToggle } = this.props;
+
+    return (
+      <h3
+        className={'time clickable' + (paused ? ' blink' : '')}
+        onClick={onToggle}
+      >
+        {this.getTimeString(time)}
+      </h3>
+    );
+  }
 }
-
-const Time = ({ time, paused, onToggle }) => (
-  <h3
-    className={'time clickable' + (paused ? ' blink' : '')}
-    onClick={onToggle}
-  >
-    {getTimeString(time)}
-  </h3>
-);
 
 export default Time;
